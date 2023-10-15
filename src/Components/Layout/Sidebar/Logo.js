e3bf89da2d6e542b4e4d44db84a7bab5c6236e93
Seo-Jooyoung <136756125+Seo-Jooyoung@users.noreply.github.com>
2023-10-15 13:19:19 +0900

    FEAT: Logo 퍼블리싱 구현

---
+++ src/Components/Layout/Sidebar/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <button className='flex justify-center items-center w-full h-[6.1875rem] px-3'>
+      <img className='w-full h-auto' src="Youth2_logo.png" alt="Youth2 logo"></img>
+    </button>
+  )
+}
+
+export default Logo
