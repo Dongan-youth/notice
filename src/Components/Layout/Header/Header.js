053422ffad233e360decb03b5dd0e3ae43b9bad3
Seo-Jooyoung <136756125+Seo-Jooyoung@users.noreply.github.com>
2023-10-22 12:54:01 +0900

    Docs: Header 컴포넌트 크기 비율 조정

--- src/Components/Layout/Header/Header.js
+++ src/Components/Layout/Header/Header.js
@@ -5,7 +5,7 @@ import ProfileBtn from './ProfileBtn.js';
 
 const Header = () => {
   return (
-    <div className='flex justify-end items-center h-[6.25rem] w-full border-b-[1px] border-[#D2D2D2]'>
+    <div className='flex justify-end items-center h-[10vh] w-full border-b-[1px] border-[#D2D2D2]'>
         <ModeBtn />
         <ProfileBtn />
     </div>
