82fff900d3808e89bb7fb71e2b5dfef4c385cce9
Seo-Jooyoung <136756125+Seo-Jooyoung@users.noreply.github.com>
2023-10-28 18:22:33 +0900

    FEAT: Header 모드 버튼 퍼블리싱 구현

---
+++ src/Components/Layout/Header/ModeBtn.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ModeBtn = () => {
+  return (
+    <div className='flex justify-center items-center w-[7.25rem] h-[65%] bg-[#F6F6F9] rounded-[0.625rem] mx-[1.88rem]'>
+      <button className='flex justify-center items-center w-[6rem] h-[3rem] bg-[#C6CCD8] rounded-[0.625rem]'>
+        <button className='flex justify-center items-center w-[3rem] h-[3rem] bg-[#FFFFFF] rounded-[0.625rem]'>
+          <img src='Light Icon.png' alt='LightMode' className='flex w-[2.2rem] h-[2.2rem] m-[0.5rem]'></img>
+        </button>
+        <button className='flex justify-center items-center w-[3rem] h-[3rem] bg-[#C6CCD8] rounded-[0.625rem]'>
+          <img src='Dark Icon.png' alt='DarkMode' className='flex w-[1.875rem] h-[1.875rem] m-[0.5rem]'></img>
+        </button>
+      </button>
+    </div>
+  )
+}
+
+export default ModeBtn
