efde04c145e3e31bd5ca329d4d50c909e4032f58
A Meaningful star <ghrlfehd123@naver.com>
2023-11-07 00:32:52 +0900

    FIX: 파일 경로 수정

--- src/Components/PC/Layout/Layout.js
+++ src/Components/PC/Layout/Layout.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import Sidebar from './Sidebar/Sidebar'
-import Main from './Main/Main'
+import React from 'react';
+import Sidebar from './Sidebar/Sidebar';
+import Main from './Main/Main';
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
-    <div>
-      <Sidebar />
-      <Main />
+    <div className='w-full min-h-screen bg-[#F6F6F9]'>
+      <Sidebar/>
+      <Main>
+        {children}
+      </Main>
     </div>
   )
 }
