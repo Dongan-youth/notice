369d2d18207d1b3fb7fc2ecef1f04a38a99f98a5
Seo-Jooyoung <136756125+Seo-Jooyoung@users.noreply.github.com>
2023-12-05 17:20:41 +0900

    FEAT: MyPage 레이아웃 퍼블리싱 구현

---
+++ src/Pages/PC/MyPageP.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../Components/PC/Layout/Layout'
+import MyPage from '../../Components/PC/MyPage/MyPage'
+
+const MyPageP = () => {
+  return (
+    <Layout>
+        <MyPage/>
+    </Layout>
+  )
+}
+
+export default MyPageP
