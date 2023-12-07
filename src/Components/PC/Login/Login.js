f4456fe23d79f12d81b1470c685553dc120140a1
A Meaningful star <ghrlfehd123@naver.com>
2023-12-07 15:09:12 +0900

    STYLE: Login 스타일 수정
    23.12.07
    
    - Login 스타일 수정

--- src/Components/PC/Login/Login.js
+++ src/Components/PC/Login/Login.js
@@ -1,40 +1,68 @@
-import React, { useState } from 'react';
-import Logo from './Logo'
-import TextField from '@mui/material/TextField'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import Logo from "./Logo";
+import TextField from "@mui/material/TextField";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
 
-  const [loginData, setLoginData]=useState({
-    id: '',
-    pw: ''
-  })
+  const [loginData, setLoginData] = useState({
+    id: "",
+    pw: "",
+  });
 
   const handleSubmit = () => {
-    console.log(loginData)
-    navigate('/')
-  }
+    console.log(loginData);
+    navigate("/");
+  };
 
   return (
-    <div className='flex flex-col justify-center items-center w-[25.5rem] h-[25.1875rem] gap-3'>
-        <Logo/>
-        <div className='flex flex-col justify-center items-center w-[19.9375rem] gap-[1.81rem]'>
-            <TextField id="outlined-basic" name='email' label='email' variant='outlined' size='small' fullWidth value={loginData.id} onChange={(e) => setLoginData({...loginData, id: e.target.value})}/>
-            <TextField id="outlined-basic" name='password' type='password' label='password' variant='outlined' size='small' fullWidth value={loginData.pw} onChange={(e) => setLoginData({...loginData, pw:e.target.value})}/>
-            <div className='flex flex-row justify-center items-center gap-2 text-[#6D7A8F] text-[0.9375rem]'>
-                <Link to={'/'} className='font-bold'>아이디찾기</Link>
-                <p>│</p>
-                <Link to={'/'} className='font-bold'>비밀번호찾기</Link>
-                <p>│</p>
-                <Link to={'/join'} className='font-bold'>회원가입</Link>
-            </div>
-            <button className='w-[19.9375rem] h-[2.5625rem] bg-[#7D8DA7] rounded-[0.4375rem]' onClick={handleSubmit}>
-              <span className='text-[#FFFFFF] text-xl font-bold font-[Inter]'>LogIn</span>
-            </button>
-        </div>
+    <div className="flex flex-col justify-center items-center gap-5 w-80">
+      <Logo />
+      <TextField
+        id="outlined-basic"
+        name="email"
+        label="email"
+        variant="outlined"
+        size="small"
+        fullWidth
+        value={loginData.id}
+        onChange={(e) => setLoginData({ ...loginData, id: e.target.value })}
+      />
+      <TextField
+        id="outlined-basic"
+        name="password"
+        type="password"
+        label="password"
+        variant="outlined"
+        size="small"
+        fullWidth
+        value={loginData.pw}
+        onChange={(e) => setLoginData({ ...loginData, pw: e.target.value })}
+      />
+      <div className="flex flex-row justify-center items-center gap-2 text-[#6D7A8F] text-[0.9375rem]">
+        <Link to={"/"} className="font-bold">
+          아이디찾기
+        </Link>
+        <p>│</p>
+        <Link to={"/"} className="font-bold">
+          비밀번호찾기
+        </Link>
+        <p>│</p>
+        <Link to={"/join"} className="font-bold">
+          회원가입
+        </Link>
+      </div>
+      <button
+        className="w-full h-10 bg-[#7D8DA7] rounded-md"
+        onClick={handleSubmit}
+      >
+        <span className="text-[#FFFFFF] text-xl font-bold font-[Inter]">
+          LogIn
+        </span>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/Pages/PC/LoginP.js
+++ src/Pages/PC/LoginP.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import Login from '../../Components/PC/Login/Login.js'
+import React from "react";
+import Login from "../../Components/PC/Login/Login.js";
 
 const LoginP = () => {
   return (
-    <div className='flex justify-center items-center w-full h-screen overflow-hidden p-3'>
-        <div className='flex justify-center items-center w-full h-full bg-[#FFFFFF] rounded-2xl'>
-            <Login/>
-        </div>
+    <div className="flex justify-center items-center w-full min-h-screen">
+      <Login />
     </div>
-  )
-}
+  );
+};
 
-export default LoginP
+export default LoginP;
