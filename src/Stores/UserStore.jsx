70a12d96d741f5fe948955db1c07f7dd07d185d1
A Meaningful star <ghrlfehd123@naver.com>
2023-12-13 22:48:44 +0900

    FEAT: zustand 사용자 store 구현
    23.12.13
    
    - user 객체 안에 사용자 정보를 담는 currentUser 변수와 로딩 관련된 isLoading 변수를 가지고 있습니다.
    - setUser 함수는 로그인 시 발생하고 사용자의 정보를 저장하는 동작을 하는 함수입니다.
    - clearUser 함수는 로그아웃 시 발생하고 사용자의 정보를 삭제하는 동작을 하는 함수입니다.

--- src/Stores/UserStore.jsx
+++ src/Stores/UserStore.jsx
@@ -1,7 +1,30 @@
 import { create } from "zustand";
 
 const UserStore = create()((set) => ({
+  user:{
+    currentUser: null,
+    isLoading: true,
+  },
 
+  // 로그인 시 사용자 정보 저장
+  setUser : (userInfo) =>
+    set((prev) => ({
+      user: {
+        ...prev.user,
+        currentUser: userInfo,
+        isLoading: false,
+      }
+    })),
+
+  // 로그아웃 시 사용자 정보 삭제
+  clearUser : () => 
+    set((prev) => ({
+      user: {
+        ...prev.user,
+        currentUser: null,
+        isLoading: false,
+      }
+    }))
 }));
 
 export default UserStore;
