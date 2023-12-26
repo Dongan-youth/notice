c2041475b960f3c5dec332ecb732025f068378da
Seo-Jooyoung <136756125+Seo-Jooyoung@users.noreply.github.com>
2023-12-26 17:14:25 +0900

    FEAT: 로그아웃 기능 추가

--- src/Components/PC/Layout/Header/HoverCard.js
+++ src/Components/PC/Layout/Header/HoverCard.js
@@ -1,7 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../../../../firebase_local";
+import UserStore from "../../../../Stores/UserStore";
+
 
 const HoverCard = ({ isHovered, setIsHovered }) => {
+  const { clearUser } = UserStore();
+  const handleLogoutBtn = () => {
+    signOut(auth).then(() => {
+      clearUser();
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
   return (
     <div
       className="absolute top-16 right-7 w-36 h-36 p-3 bg-[#F6F6F9] rounded-xl shadow-md"
@@ -14,12 +27,12 @@ const HoverCard = ({ isHovered, setIsHovered }) => {
             MyPage
           </span>
         </Link>
-        <div className="flex gap-3 p-3 items-center rounded-b-xl cursor-pointer hover:bg-gray-50">
+        <button onClick={ handleLogoutBtn } className="flex gap-3 p-3 items-center rounded-b-xl cursor-pointer hover:bg-gray-50">
           <img src="LogIn.png" alt="Login" className="w-6" />
           <span className="col-span-3 text-[#ABABAB] text-xm font-bold font-[Inter]">
             LogOut
           </span>
-        </div>
+        </button>
       </div>
     </div>
   );
