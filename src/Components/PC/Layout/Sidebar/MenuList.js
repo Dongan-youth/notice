22b7c1d259cbffd3002f3007367cea1040712152
A Meaningful star <ghrlfehd123@naver.com>
2023-12-27 00:22:34 +0900

    FEAT: 페이지 이동 및 페이지 이동에 따른 스타일 수정
    23.12.27
    
    - 로고를 눌렀을 때 페이지 main으로 이동
    - 페이지 이동에 따른 스타일 적용
    - hover 시 이상한 UI 해결

--- src/Components/PC/Layout/Sidebar/MenuList.js
+++ src/Components/PC/Layout/Sidebar/MenuList.js
@@ -1,18 +1,26 @@
-import React from 'react'
-import Menu from './Menu.js'
+import React from "react";
+import Menu from "./Menu.js";
 
 const MenuList = () => {
   const menuData = [
-    { imageURL: 'Menu1.png', text: '온라인 주보' },
-    { imageURL: 'Menu2.png', text: '사진 및 동영상' }
+    { id: "/online-notice", imageURL: "Menu1.png", text: "온라인 주보" },
+    { id: "/photos-and-videos", imageURL: "Menu2.png", text: "사진 및 동영상" },
   ];
 
   return (
-    <div className='row-span-2 grid grid-rows-2 gap-3 my-3'>
-      <Menu MenuImageURL={menuData[0].imageURL} MenuName={menuData[0].text}/>
-      <Menu MenuImageURL={menuData[1].imageURL} MenuName={menuData[1].text}/>
+    <div className="row-span-2 grid grid-rows-2 gap-3 my-3">
+      <Menu
+        PageUrl={menuData[0].id}
+        MenuImageURL={menuData[0].imageURL}
+        MenuName={menuData[0].text}
+      />
+      <Menu
+        PageUrl={menuData[1].id}
+        MenuImageURL={menuData[1].imageURL}
+        MenuName={menuData[1].text}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default MenuList
+export default MenuList;
