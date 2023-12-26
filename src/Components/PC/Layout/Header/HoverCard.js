212e8e9061dacd60ec9f5b155dcbfe8c34bd22e9
A Meaningful star <ghrlfehd123@naver.com>
2023-12-26 16:43:16 +0900

    FIX: 페이지 이동을 위한 버튼
    23.12.26
    
    - 페이지 이동을 위해 'div' -> 'Link'로 수정

--- src/Components/PC/Layout/Header/HoverCard.js
+++ src/Components/PC/Layout/Header/HoverCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HoverCard = ({ isHovered, setIsHovered }) => {
   return (
@@ -7,18 +8,18 @@ const HoverCard = ({ isHovered, setIsHovered }) => {
       onMouseLeave={() => setIsHovered({...isHovered, hoverCard: false })}
     >
       <div className="w-full h-full rounded-xl grid grid-rows-2 bg-white">
-        <div className="flex gap-3 p-3 items-center rounded-t-xl cursor-pointer hover:bg-gray-50">
+        <Link  className="flex gap-3 p-3 items-center rounded-t-xl cursor-pointer hover:bg-gray-50">
           <img src="Mypage.png" alt="Mypage" className="w-6" />
           <span className="col-span-3 text-[#ABABAB] text-xm font-bold font-[Inter]">
             MyPage
           </span>
-        </div>
-        <div className="flex gap-3 p-3 items-center rounded-b-xl cursor-pointer hover:bg-gray-50">
+        </Link>
+        <Link className="flex gap-3 p-3 items-center rounded-b-xl cursor-pointer hover:bg-gray-50">
           <img src="LogIn.png" alt="Login" className="w-6" />
           <span className="col-span-3 text-[#ABABAB] text-xm font-bold font-[Inter]">
             LogOut
           </span>
-        </div>
+        </Link>
       </div>
     </div>
   );
