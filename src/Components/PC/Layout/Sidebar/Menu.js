0c60f2961a892b599ba8c3c862f1e62e36d2a343
A Meaningful star <ghrlfehd123@naver.com>
2024-01-03 19:32:09 +0900

    STYLE: Sidebar 스타일 수정
    24.01.03
    
    - Sidebar 반응형을 생각하여 너비 또는 높이 값을 고정해야할 부분은 고정

--- src/Components/PC/Layout/Sidebar/Menu.js
+++ src/Components/PC/Layout/Sidebar/Menu.js
@@ -7,24 +7,22 @@ const Menu = ({ MenuImageURL, MenuName, PageUrl }) => {
   return (
     <>
       {PageUrl === location.pathname ? (
-        <Link
-          to={PageUrl}
-          className="grid grid-cols-4 gap-3 p-3 items-center rounded-xl bg-[#F6F6F9] border-[#CACAD0] border-2 border-solid"
-        >
-          <img src={MenuImageURL} alt="Menu" className="w-7" />
-          <span className="col-span-3 text-[#7D8DA7] text-lg text-center font-semibold font-[Inter]">
-            {MenuName}
-          </span>
+        <Link to={PageUrl}>
+          <div className="flex flex-row items-center py-3 rounded-xl bg-[#F6F6F9] border-[#CACAD0] border-2 border-solid">
+            <img src={MenuImageURL} alt="Menu" className="w-auto h-9 px-5" />
+            <span className="text-[#7D8DA7] px-2 text-lg text-center font-semibold font-[Inter]">
+              {MenuName}
+            </span>
+          </div>
         </Link>
       ) : (
-        <Link
-          to={PageUrl}
-          className="grid grid-cols-4 gap-3 p-3 items-center rounded-xl border-2 border-white hover:bg-[#F6F6F9] hover:border-[#CACAD0] hover:border-2 hover:border-solid"
-        >
-          <img src={MenuImageURL} alt="Menu" className="w-7" />
-          <span className="col-span-3 text-[#7D8DA7] text-lg text-center font-semibold font-[Inter]">
-            {MenuName}
-          </span>
+        <Link to={PageUrl}>
+          <div className="flex flex-row items-center py-3 rounded-xl border-2 border-white hover:bg-[#F6F6F9] hover:border-[#CACAD0] hover:border-2 hover:border-solid">
+            <img src={MenuImageURL} alt="Menu" className="w-auto h-9 px-5" />
+            <span className="text-[#7D8DA7] px-2 text-lg text-center font-semibold font-[Inter]">
+              {MenuName}
+            </span>
+          </div>
         </Link>
       )}
     </>
