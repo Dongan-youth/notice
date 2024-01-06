50ecf747b5de46e4325303397cff596bf3c81ac5
A Meaningful star <ghrlfehd123@naver.com>
2024-01-06 18:31:21 +0900

    STYLE: 'absolute'로 인한 문제 해결
    24.01.06
    
    - 'absolute'로 인한 눌러지지 않는 문제 해결을 위해 'z-index' 추가하여 해결

--- src/Components/PC/Layout/Sidebar/Sidebar.js
+++ src/Components/PC/Layout/Sidebar/Sidebar.js
@@ -4,7 +4,7 @@ import MenuList from "./MenuList";
 
 const Sidebar = () => {
   return (
-    <div className="absolute top-0 left-0 p-3 w-72 h-full min-h-screen">
+    <div className="absolute top-0 left-0 p-3 w-72 h-full min-h-screen z-10">
       <div className="bg-white w-full h-full flex flex-col overflow-y-auto py-4 px-3 rounded-2xl">
         <Logo />
         <MenuList />
